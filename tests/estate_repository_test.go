6dbd503fdb5e910653c94133c24f3b438bc58bdf
HOL USER <holuser@ip-192-168-0-102.ap-southeast-3.compute.internal>
2024-10-02 11:46:01 +0700

    add repo test

---
+++ tests/estate_repository_test.go
@@ -0,0 +1,66 @@
+package tests
+
+import (
+    "testing"
+    "sawitpro-recruitment/models"
+    "github.com/golang/mock/gomock"
+    "github.com/google/uuid"
+    "github.com/stretchr/testify/assert"
+)
+
+func TestCreateEstate(t *testing.T) {
+    ctrl := gomock.NewController(t)
+    defer ctrl.Finish()
+
+    mockRepo := NewMockEstateRepository(ctrl)
+
+    estate := &models.Estate{
+        ID:     uuid.New(),
+        Width:  100,
+        Length: 200,
+    }
+
+    mockRepo.EXPECT().CreateEstate(estate).Return(nil)
+
+    err := mockRepo.CreateEstate(estate)
+    assert.NoError(t, err)
+}
+
+func TestGetEstateByID(t *testing.T) {
+    ctrl := gomock.NewController(t)
+    defer ctrl.Finish()
+
+    mockRepo := NewMockEstateRepository(ctrl)
+
+    estateID := uuid.New()
+    expectedEstate := &models.Estate{
+        ID:     estateID,
+        Width:  100,
+        Length: 200,
+    }
+
+    mockRepo.EXPECT().GetEstateByID(estateID).Return(expectedEstate, nil)
+
+    estate, err := mockRepo.GetEstateByID(estateID)
+    assert.NoError(t, err)
+    assert.Equal(t, expectedEstate, estate)
+}
+
+func TestGetEstateStats(t *testing.T) {
+    ctrl := gomock.NewController(t)
+    defer ctrl.Finish()
+
+    mockRepo := NewMockEstateRepository(ctrl)
+
+    estateID := uuid.New()
+    expectedCount, expectedMax, expectedMin, expectedMedian := 10, 50, 5, 25
+
+    mockRepo.EXPECT().GetEstateStats(estateID).Return(expectedCount, expectedMax, expectedMin, expectedMedian, nil)
+
+    count, max, min, median, err := mockRepo.GetEstateStats(estateID)
+    assert.NoError(t, err)
+    assert.Equal(t, expectedCount, count)
+    assert.Equal(t, expectedMax, max)
+    assert.Equal(t, expectedMin, min)
+    assert.Equal(t, expectedMedian, median)
+}
---
+++ tests/tree_repository_test.go
@@ -0,0 +1,69 @@
+package tests
+
+import (
+    "testing"
+    "sawitpro-recruitment/models"
+    "github.com/golang/mock/gomock"
+    "github.com/google/uuid"
+    "github.com/stretchr/testify/assert"
+)
+
+func TestAddTreeToEstate(t *testing.T) {
+    ctrl := gomock.NewController(t)
+    defer ctrl.Finish()
+
+    mockRepo := NewMockTreeRepository(ctrl)
+
+    tree := &models.Tree{
+        ID:       uuid.New(),
+        EstateID: uuid.New(),
+        X:        10,
+        Y:        20,
+        Height:   30,
+    }
+
+    mockRepo.EXPECT().AddTreeToEstate(tree).Return(nil)
+
+    err := mockRepo.AddTreeToEstate(tree)
+    assert.NoError(t, err)
+}
+
+func TestGetTreeByCoordinates(t *testing.T) {
+    ctrl := gomock.NewController(t)
+    defer ctrl.Finish()
+
+    mockRepo := NewMockTreeRepository(ctrl)
+
+    estateID := uuid.New()
+    expectedTree := &models.Tree{
+        ID:     uuid.New(),
+        X:      10,
+        Y:      20,
+        Height: 30,
+    }
+
+    mockRepo.EXPECT().GetTreeByCoordinates(estateID, 10, 20).Return(expectedTree, nil)
+
+    tree, err := mockRepo.GetTreeByCoordinates(estateID, 10, 20)
+    assert.NoError(t, err)
+    assert.Equal(t, expectedTree, tree)
+}
+
+func TestGetTreesByEstateID(t *testing.T) {
+    ctrl := gomock.NewController(t)
+    defer ctrl.Finish()
+
+    mockRepo := NewMockTreeRepository(ctrl)
+
+    estateID := uuid.New()
+    expectedTreeHeights := map[string]int{
+        "10,20": 30,
+        "15,25": 35,
+    }
+
+    mockRepo.EXPECT().GetTreesByEstateID(estateID).Return(expectedTreeHeights, nil)
+
+    treeHeights, err := mockRepo.GetTreesByEstateID(estateID)
+    assert.NoError(t, err)
+    assert.Equal(t, expectedTreeHeights, treeHeights)
+}
