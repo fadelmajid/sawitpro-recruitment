50cd93b583492bfe699b4c1d6f4c67837cbc4f82
HOL USER <holuser@HOLs-MacBook-Pro.local>
2024-10-03 15:29:15 +0700

    update server.go

--- cmd/server.go
+++ cmd/server.go
@@ -1,63 +1,50 @@
 package main
 
 import (
-    "net/http"
-    "github.com/labstack/echo/v4"
-    "sawitpro-recruitment/generated"
-    "sawitpro-recruitment/repositories"
-    "github.com/google/uuid" // Import the UUID package
+	"sawitpro-recruitment/generated"
+	"sawitpro-recruitment/handlers"
+	"sawitpro-recruitment/repositories"
+	"strconv"
+
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
 )
 
 type Server struct {
-    estateRepo repositories.EstateRepository
-    treeRepo   repositories.TreeRepository
+	estateHandler *handlers.EstateHandler
+	droneHandler  *handlers.DroneHandler
+	treeHandler   *handlers.TreeHandler
 }
 
 func NewServer(estateRepo repositories.EstateRepository, treeRepo repositories.TreeRepository) *Server {
-    return &Server{
-        estateRepo: estateRepo,
-        treeRepo:   treeRepo,
-    }
+	return &Server{
+		estateHandler: handlers.NewEstateHandler(estateRepo),
+		droneHandler:  handlers.NewDroneHandler(treeRepo, estateRepo),
+		treeHandler:   handlers.NewTreeHandler(treeRepo, estateRepo),
+	}
 }
 
 func (s *Server) PostEstate(ctx echo.Context) error {
-    var estate generated.Estate
-    if err := ctx.Bind(&estate); err != nil {
-        errMsg := err.Error() // Assign error message to a variable
-        return ctx.JSON(http.StatusBadRequest, generated.Error{Message: &errMsg})
-    }
-
-    // Add business logic to create an estate
-    // Example: s.estateRepo.CreateEstate(estate)
-
-    return ctx.JSON(http.StatusCreated, estate)
+	return s.estateHandler.CreateEstate(ctx)
 }
 
 func (s *Server) GetEstateIdDronePlan(ctx echo.Context, id uuid.UUID, params generated.GetEstateIdDronePlanParams) error {
-    // Add business logic to calculate the drone's total travel distance
-    // Example: dronePlan := s.droneHandler.CalculateDronePlan(id, params.MaxDistance)
-
-    var dronePlan generated.DronePlan
-    return ctx.JSON(http.StatusOK, dronePlan)
+	ctx.SetParamNames("id")
+	ctx.SetParamValues(id.String())
+	if params.MaxDistance != nil {
+		ctx.QueryParams().Set("max_distance", strconv.Itoa(*params.MaxDistance))
+	}
+	return s.droneHandler.CalculateDronePlanWithLimit(ctx)
 }
 
 func (s *Server) GetEstateIdStats(ctx echo.Context, id uuid.UUID) error {
-    // Add business logic to get stats of trees in an estate
-    // Example: stats := s.treeRepo.GetEstateStats(id)
-
-    var stats generated.EstateStats
-    return ctx.JSON(http.StatusOK, stats)
+	ctx.SetParamNames("id")
+	ctx.SetParamValues(id.String())
+	return s.estateHandler.GetEstateStats(ctx)
 }
 
 func (s *Server) PostEstateIdTree(ctx echo.Context, id uuid.UUID) error {
-    var tree generated.Tree
-    if err := ctx.Bind(&tree); err != nil {
-        errMsg := err.Error() // Assign error message to a variable
-        return ctx.JSON(http.StatusBadRequest, generated.Error{Message: &errMsg})
-    }
-
-    // Add business logic to add a tree to an estate
-    // Example: s.treeRepo.AddTreeToEstate(id, tree)
-
-    return ctx.JSON(http.StatusCreated, tree)
+	ctx.SetParamNames("id")
+	ctx.SetParamValues(id.String())
+	return s.treeHandler.AddTreeToEstate(ctx)
 }
